de3626f1890911cfa113637503d813d850a71da4
JunSeong_Park <ttotto0608@naver.com>
2023-03-13 00:57:05 +0900

    basic setting

---
+++ src/main/java/com/example/csrex/CsrExApplication.java
@@ -0,0 +1,13 @@
+package com.example.csrex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsrExApplication.class, args);
+    }
+
+}
